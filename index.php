27036649d419810c7c0d612464cf4856dbddbad6
Andrew Sharon <andrewsharon@gmx.com>
2022-03-07 21:01:55 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,45 @@
+﻿<!DOCTYPE html>
+<!--TheFreeElectron 2015, http://www.instructables.com/member/TheFreeElectron/ -->
+
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Raspberry Pi Gpio</title>
+    </head>
+
+    <body>
+    <style>
+    background-color: black;
+    h1 {text-align: center;}
+    </style>
+    <!-- On/Off button's picture -->
+	<?php
+	$val_array = array(0);
+	//this php script generate the first page in function of the file
+
+		//set the pin's mode to output and read them
+		system("gpio mode 0 out");
+		exec ("gpio read 0", $val_array[0], $return );
+
+	//for loop to read the value
+	$i =0;
+
+		//if off, create an image named for the pin and with the appropriate color
+    //image on the page. Also calls change_pin() from script.js if the image
+    //is clicked.
+    //echo ("<h1>Fan Power</h1><br>");
+    echo("<img id='fanpower' src='data/img/fanpower.png'/><br>");
+    if ($val_array[$i][0] == 0 ) {
+			echo ("<img id='button_".$i."' src='data/img/red/red_".$i.".jpg' class = 'center' onclick='change_pin (".$i.");'/>");
+		}
+		//if on, do the same with a green image
+		if ($val_array[$i][0] == 1 ) {
+			echo ("<img id='button_".$i."' src='data/img/green/green_".$i.".jpg' class = 'center' onclick='change_pin (".$i.");'/>");
+		}
+
+	?>
+
+	<!-- javascript -->
+	<script src="script.js"></script>
+    </body>
+</html>
---
+++ script.js
@@ -0,0 +1,80 @@
+﻿//TheFreeElectron 2015, http://www.instructables.com/member/TheFreeElectron/
+//JavaScript, uses pictures as buttons, sends and receives values to/from the Rpi
+//These are all the buttons
+//these lines grab elements from index.php based on their name and assigns them
+//to variables
+var button_0 = document.getElementById("button_0");
+
+//Create an array for easy access later
+var Buttons = [ button_0];
+
+//This function is asking for gpio.php, receiving datas and updating the index.php pictures
+function change_pin ( pic ) {
+var data = 0;
+//send the pic number to gpio.php for changes
+//this is the http request
+	var request = new XMLHttpRequest();
+	// request.open calls gpio.php specifying a picture number
+	// the corresponding pin is toggled, and its final state is returned
+	// in responseText, used in the onreadystatechange function
+	// true at the end specifies an asynchronous request, (which is default)
+	request.open( "GET" , "gpio.php?pic=" + pic, true);
+	// Send the request
+	request.send(null);
+	//receiving informations
+	/*  XMLHttpRequest readyState codes
+	0		The request is not initialized.
+	1		The request has been set up.
+	2		The request has been sent.
+	3		The request is in process.
+	4		The request is completed.
+
+	status codes
+	200 OK
+	403 Forbidden
+	404 Page not found
+	500 Internal Server Error
+
+	onreadystatechange specifies a function to be executed every time the status
+	of the xmlhttprequest object changes: When readyState is 4 and status is 200
+	the response is ready.
+
+	The responseText property returns the server response as a text string.
+
+	localeCompare compares two strings. If they are equivalent, it returns 0
+
+
+	*/
+	request.onreadystatechange = function () {
+		if (request.readyState == 4 && request.status == 200) {
+			data = request.responseText;
+			// the responseText should be "0" or "1"
+			//update the index pic
+			if ( !(data.localeCompare("0")) ){
+				Buttons[pic].src = "data/img/red/red_"+pic+".jpg";
+			}
+			else if ( !(data.localeCompare("1")) ) {
+				Buttons[pic].src = "data/img/green/green_"+pic+".jpg";
+			}
+			else if ( !(data.localeCompare("fail"))) {
+				alert ("Something went wrong!" );
+				return ("fail");
+			}
+			else {
+				alert ("Something went wrong!" );
+				return ("fail");
+			}
+		}
+		//test if fail
+		else if (request.readyState == 4 && request.status == 500) {
+			alert ("server error");
+			return ("fail");
+		}
+		//else
+		else if (request.readyState == 4 && request.status != 200 && request.status != 500 ) {
+			alert ("Something went wrong!");
+			return ("fail"); }
+	}
+
+return 0;
+}
